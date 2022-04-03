fb8803d8651c85a5332a946ca788ed6be0cdfb6f
David Kern <david33belle@gmail.com>
2022-04-03 21:39:25 +0200

    refactor: code

--- index.js
+++ index.js
@@ -12,27 +12,25 @@ app.use(cors());
 app.get('/output/:id', (req, res) => {
   const filePath = '.' + req.url;
 
-  fs.readFile(filePath, function (error, content) {
-    res.writeHead(200, { 'Access-Control-Allow-Origin': '*' });
-    if (error) {
-      if (error.code == 'ENOENT') {
-        res.end(content, 'utf-8');
+  if (filePath.includes('.ts')) {
+    const stream = fs.createReadStream(filePath, { bufferSize: 64 * 1024 });
+    stream.pipe(res);
+  } else {
+    fs.readFile(filePath, function (error, content) {
+      res.writeHead(200, { 'Access-Control-Allow-Origin': '*' });
+      if (error) {
+        if (error.code == 'ENOENT') {
+          res.end(content, 'utf-8');
+        }
+        else {
+          res.writeHead(500);
+          res.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
+          res.end();
+        }
       }
-      else {
-        res.writeHead(500);
-        res.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
-        res.end();
-      }
-    }
-    else {
-      if (filePath.includes('.ts')) {
-        const stream = fs.createReadStream(filePath,{ bufferSize: 64 * 1024 });
-        stream.pipe(res);
-      } else {
-        res.end(content, 'utf-8');
-      }
-    }
-  });
+      res.end(content, 'utf-8'); 
+    });
+  }
 
 });
 
