9223fdac31d5179f85256f6370d6294ae93c61ac
davidkern13 <david33belle@gmail.com>
2022-05-20 11:40:05 +0200

    feat: youtube live stream

---
+++ nodejs/youtube.js
@@ -0,0 +1,15 @@
+const ffmpeg = require('fluent-ffmpeg');
+const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg');
+
+ffmpeg.setFfmpegPath(ffmpegInstaller.path);
+
+ffmpeg('http://localhost:8000/vod/output.m3u8')
+    .addOption('-vcodec', 'libx264')
+    .addOption('-acodec', 'aac')
+    .addOption('-crf', 15)
+    .addOption('-aspect', '240:128')
+    .addOption('-pix_fmt', 'yuv420p')
+    .addOption('-f', 'flv')
+    .output('rtmp://a.rtmp.youtube.com/live2/' + 'jeg8-1raq-8971-vuvv-baa3').on('end', () => {
+        console.log('end');
+    }).run();
